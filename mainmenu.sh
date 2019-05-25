f01c7870ee5f8dd129cdf282fc1260fea6e0a3d1
Michael Tang <michaelandrewtang@gmail.com>
2019-05-25 17:01:42 -0400

    Create Menu

---
+++ mainmenu.sh
@@ -0,0 +1,27 @@
+clear
+printf "\n\n\n\n\n*********** WELCOME TO GPLs OFFLINE ACCOUNT INQUIRY**************\n"
+printf "*********************** MENU   **********************************\n"
+printf "*********** 1) Import  Current Customer Data ********************\n"
+printf "*********** 2) Search By Customer Account #  ********************\n"
+printf "*********** 3) Search By Customer Name  *************************\n"
+printf "*****************************************************************\n\n"
+
+read -p  "        Enter your selection:        "   menu_selection
+
+case $menu_selection in
+
+        "1")
+                clear
+                printf " Importing customer data....\n"
+                ;;
+        "2")
+                clear
+                read -p " Enter Your 7 Digit Customer Account # "  account_number
+                ;;
+        q|Q)
+                exit
+                ;;
+        *)
+                printf " Please select either 1,2,3 or Q to quit \n"
+                ;;
+esac
